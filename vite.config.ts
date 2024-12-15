3c4c1b653549512913b895a8a197c9d0e31c2820
Kunal Umap <kunalgumap@gmail.com>
2024-12-15 22:48:52 +0530

    Config add

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+        modifyVars: {
+          // Variable custimization
+        },
+        additionalData: '@import "antd/dist/antd.variable.less";'
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
