c72df08a647ca9da3ae549767f67b66a461604ac
Omshree Kenjale <omshreekenjale.ok@gmail.com>
2025-01-07 16:30:08 +0530

    changes amde to layout.tsx file

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -6,20 +6,14 @@ import { LayoutProps } from "antd";
 const Layout: React.FC<LayoutProps> = ({}) => {
   return (
     <>
-      {/* Content Container */}
       <div className={styles["layout-container"]}>
-        {/* Left Section: Text */}
         <div className={styles["text-section"]}>
           <p>Buy, Sell, Rent,</p>
           <p>Property Securely!</p>
         </div>
-
-        {/* Right Section: Login Box */}
         <div className={styles["image-section"]}>
           <div className={styles["placeholder-box"]}>
-            {/* Overlapping Title Box */}
             <div className={styles["title-box"]}>Login</div>
-            {/* Render children here for nested routes */}
             <Outlet />
           </div>
         </div>
