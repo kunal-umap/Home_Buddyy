8c865104cd282534be6ce3097949c0c4076d9bf7
Omshree Kenjale <omshreekenjale.ok@gmail.com>
2025-01-15 18:10:48 +0530

    Resolve merge conflicts and finalize changes

--- src/router.tsx
+++ src/router.tsx
@@ -1,48 +1,28 @@
-import { createBrowserRouter, Outlet, RouteObject } from "react-router-dom";
+import { createBrowserRouter, RouteObject } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
-
+import Layout from "./components/layout/Layout";
+import SignupPage from "./pages/loginSignup/SignupPage";
+import LoginPage from "./pages/loginSignup/LoginPage";
 
 const routes: RouteObject[] = [
-    {
-        path: '/',
-        element: <HomePage />,
-    },
-    {
-        path: '/auth',
-        element: <div>About
-            <Outlet />
-        </div>,
-        children: [
-            {
-                path: 'login',
-                element: <div>1</div>
-            },
-            {
-                path: '2',
-                element: <div>2</div>
-            }
-        ]
-    }
-    
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/loginSignup", // Parent route
+    element: <Layout>{null}</Layout>, // Wraps child routes with the Layout component
+    children: [
+      {
+        path: "login", // Renders LoginPage at "/loginSignup/login"
+        element: <LoginPage />,
+      },
+      {
+        path: "signup", // Renders SignupPage at "/loginSignup/signup"
+        element: <SignupPage />,
+      },
+    ],
+  },
 ];
 
 export const router = createBrowserRouter(routes);
-
-
-
-/* 
-
-    Folder (LoginSignupPage.tsx & css -> pages) -> layout1 -> outlet tag
-    components -> loginForm -> LoginFrom.tsx, LoginFrom.module.css 
-    components -> signupForm -> SignupFrom.tsx, SignupFrom.module.css
-
-    Pages -> Folder - MoreDetailsPage[ 
-    
-    -> MoreDetailsPage.tsx, MoreDetailsPage.module.css,
-    ->  MoreDetailsForm -> [MoreDetailsForm.tsx, MoreDetailsForm.module.css]
-    ]
-
-
-    Font sizes, 3,4 Colours , Spacing 
-
-*/
